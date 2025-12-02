102d95d0a6f0120fcfbca27a19b3bc34b9e5e426
Alex Hunter <alex@alexhunter.net>
2025-12-02 09:35:10 +0000

    Updated mobile navigation layout. Fixed json on index page. Updated robots.txt.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,22 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 export default defineConfig({
   site: 'https://www.nationaldebtservice.co.uk',
-  integrations: [sitemap()],
+  integrations: [
+    sitemap(), 
+    robotsTxt({
+      policy: [
+        {
+          userAgent: '*', 
+          allow: '/',
+        },
+      ],
+      sitemap: true, 
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
